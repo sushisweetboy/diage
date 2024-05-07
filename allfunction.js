51bc94ebc431e32c88505ad6f24f4e39553f82a1
sushisweetboy <153652802+sushisweetboy@users.noreply.github.com>
2024-05-08 03:18:43 +0700

    Add files via upload

---
+++ allfunction.js
@@ -0,0 +1,25 @@
+let re =document.querySelector('.restart');
+
+function clickkk(){
+ window.location.href="firstpage.html";
+}
+
+
+function firstpage(){
+    window.location.href="question.html";
+}
+
+re.addEventListener('click',restart);
+function restart(){
+   console.log('restart worked');
+   window.location.href="level2.html";
+}
+function leadtolevel2(){
+    window.location.href="introductionlevel2.html";
+}
+function gotolevel2(){
+    window.location.href="level2.html";
+}
+function backtolevel1(){
+    window.location.href="question.html";
+}
